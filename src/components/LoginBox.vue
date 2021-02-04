37f3aecb4cd3159cd0c40c5dd9d38aa837823e0c
Marcelo Martínez <marcelo2m1097@gmail.com>
2021-02-03 20:57:44 -0600

    Add the login view

---
+++ src/components/LoginBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container grey lighten-3 z-depth-2"
+    style="margin-top: 5%; padding-top: 25px; padding-left: 25px; padding-right: 25px; padding-bottom: 25px">
+    <div class="row">
+      <form>
+        <div class="col s12 center-align">
+          <img src="../assets/logo-tm-transparente.png" />
+        </div>
+        <div class="col s12 input-field">
+          <input type="text" class="validate" id="username" required>
+          <label for="username">Username</label>
+        </div>
+        <div class="col s12 input-field">
+          <input type="password" class="validate" id="password" required>
+          <label for="password">Password</label>
+        </div>
+        <div class="col s12 center-align">
+          <button type="submit" class="btn blue darken-2 waves-effect">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  template: 'loginbox'
+}
+</script>
+
+<style scoped>
+form div img {
+  height: 200px;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <div class="nav-wrapper blue darken-2">
+      <a href="#">
+        <img src="../assets/logo-tm-transparente.png" />
+      </a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="https://tinmarin.org">Sitio web</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  template: 'navbar'
+}
+</script>
+
+<style scoped>
+nav ul {
+  text-transform: uppercase;
+}
+
+img {
+  height: 100%;
+}
+</style>
