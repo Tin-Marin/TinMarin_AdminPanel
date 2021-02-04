852c8fc8453f6d3d4a6fe46f1a28d395b6f3c951
Marcelo Martínez <marcelo2m1097@gmail.com>
2021-02-03 21:49:09 -0600

    Make a successful login request and receives token as response

--- src/components/LoginBox.vue
+++ src/components/LoginBox.vue
@@ -2,20 +2,20 @@
   <div class="container grey lighten-3 z-depth-2"
     style="margin-top: 5%; padding-top: 25px; padding-left: 25px; padding-right: 25px; padding-bottom: 25px">
     <div class="row">
-      <form>
+      <form @submit.prevent="login">
         <div class="col s12 center-align">
           <img src="../assets/logo-tm-transparente.png" />
         </div>
         <div class="col s12 input-field">
-          <input type="text" class="validate" id="username" required>
+          <input type="text" class="validate" id="username" v-model="username" required>
           <label for="username">Username</label>
         </div>
         <div class="col s12 input-field">
-          <input type="password" class="validate" id="password" required>
+          <input type="password" class="validate" id="password" v-model="password" required>
           <label for="password">Password</label>
         </div>
         <div class="col s12 center-align">
-          <button type="submit" class="btn blue darken-2 waves-effect">Login</button>
+          <button type="submit" class="btn blue darken-2 waves-effect" @click="login">Login</button>
         </div>
       </form>
     </div>
@@ -23,8 +23,31 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  template: 'loginbox'
+  template: 'loginbox',
+  data () {
+    return {
+      username: '',
+      password: '',
+      endpoint: 'https://tin-marin-app.herokuapp.com/api/v1/auth/login',
+      datos: ''
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        const { data } = await axios.post(this.endpoint, {
+          username: this.username,
+          password: this.password
+        })
+        this.datos = data.token
+        console.log(data.token)
+      } catch (error) {
+        throw new Error('Something went wrong. Try later.')
+      }
+    }
+  }
 }
 </script>
 
