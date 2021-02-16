dd8925b772eec4aa1239729e203579d76f63f98a
Marcelo Martínez <marcelo2m1097@gmail.com>
2021-02-15 23:24:28 -0600

    Change buttons to save, delete, and back for icons

--- src/components/home/SuggestionTypeTable.vue
+++ src/components/home/SuggestionTypeTable.vue
@@ -24,11 +24,11 @@
         </tr>
         <tr v-show="editing" v-for="field of suggestionTypes" :key="field.id">
           <td>
-            <a class="waves-effect black btn" @click="startEditing">back</a>
+            <a @click="startEditing"><i class="material-icons black-text">arrow_back</i></a>
             <br>
-            <a class="waves-effect green btn" @click="updateSuggestionType(field)">save</a>
+            <a @click="updateSuggestionType(field)"><i class="material-icons green-text">check</i></a>
             <br>
-            <a class="waves-effect red btn" @click="deleteSuggestionType(field)">delete</a>
+            <a @click="deleteSuggestionType(field)"><i class="material-icons red-text">clear</i></a>
           </td>
           <td>{{ field._id }}</td>
           <td><input v-model="field.name"/></td>
